7de0af084820df7bf352c706c32d6cc2f2fe0b63
JonathanFoxJMLabs <136120059+JonathanFoxJMLabs@users.noreply.github.com>
2024-06-02 14:12:49 -0400

    added functionality for adding and removing from the database

---
+++ DataCare/myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Person(models.Model):
+    P_ID = models.AutoField(primary_key=True)
+    P_FName = models.CharField(max_length=50)
+    P_MName = models.CharField(max_length=50, blank=True, null=True)
+    P_LName = models.CharField(max_length=50)
+    Age = models.IntegerField()
+    Person_Type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.P_FName} {self.P_LName}"
