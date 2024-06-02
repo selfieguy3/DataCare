f88dbd6c9d891ebe9c509219e9d5b9ff97f2f346
Like4Boss <joonsuseo@gmail.com>
2024-06-02 16:26:38 -0400

    Edited Person (inheritance), Created Child, Staff, Parent, Child-Staff, Parent-Child
    For future reference:
    Child-Staff is needed because there are multiple staffs who each supervises multiple children.
    Parent-Child, likewise, there exists more than one parent who have one or more children as well.

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,61 @@ class Person(models.Model):
     P_MName = models.CharField(max_length=50, blank=True, null=True)
     P_LName = models.CharField(max_length=50)
     Age = models.IntegerField()
-    Person_Type = models.CharField(max_length=50)
+    PERSON_TYPE = [
+        ('staff', 'Staff'),
+        ('child', 'Child'),
+        ('parent', 'Parent')
+    ]
+    Person_Type = models.CharField(max_length=10, choices=PERSON_TYPE, default='staff')
+# default is staff~
 
     def __str__(self):
         return f"{self.P_FName} {self.P_LName}"
+
+class Staff(Person):
+    STAFF_ID = models.AutoField(primary_key=True)
+    Employee_Salary = models.DecimalField(max_digits=10, decimal_places=2)
+    ZipCode = models.IntegerField()
+    Phone_Number = models.CharField(max_length=15)
+    Street_Address = models.CharField(max_length=100)
+    City = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    Email = models.EmailField()
+
+    def __str__(self):
+        return f"Staff: {self.P_FName} {self.P_LName}, Staff ID: {self.STAFF_ID}"
+
+class Child(Person):
+    CHILD_ID = models.AutoField(primary_key=True)
+    HEALTH_RECORD_ID = models.IntegerField()
+    Date_of_Birth = models.DateField()
+    AVERAGE_COST = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Child: {self.P_FName} {self.P_LName}, Child ID: {self.CHILD_ID}"
+
+class Parent(Person):
+    PARENT_ID = models.AutoField(primary_key=True)
+    Phone_Number = models.CharField(max_length=15)
+    Street_Address = models.CharField(max_length=100)
+    ZipCode = models.IntegerField()
+    City = models.CharField(max_length=50)
+    State = models.CharField(max_length=50)
+    Email = models.EmailField()
+
+    def __str__(self):
+        return f"Parent: {self.P_FName} {self.P_LName}, Parent ID: {self.PARENT_ID}"
+
+class ParentChild(models.Model):
+    PARENT_ID = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    CHILD_ID = models.ForeignKey(Child, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Parent ID: {self.PARENT_ID}, Child ID: {self.CHILD_ID}"
+
+class ChildStaff(models.Model):
+    STAFF_ID = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    CHILD_ID = models.ForeignKey(Child, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Staff ID: {self.STAFF_ID}, Child ID: {self.CHILD_ID}"
